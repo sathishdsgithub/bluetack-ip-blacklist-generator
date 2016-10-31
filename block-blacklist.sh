fff2d83f641d0927621941f0935070ace502a90a
Mentor <mentor@palokaj.co>
2016-10-31 13:15:16 +0100

    Stanislav suggested some great fixes and improvements

--- block-blacklist.sh
+++ block-blacklist.sh
@@ -1,3 +1,4 @@
+sudo apt-get update && apt-get install -y iprange # Comment this out if you have installed iprange
 curl -s https://www.iblocklist.com/lists.php \
 | grep -A 2 Bluetack \
 | sed -n "s/.*value='\(http:.*\)'.*/\1/p" \
@@ -6,22 +7,24 @@ curl -s https://www.iblocklist.com/lists.php \
 | egrep -v '^#' > blacklist
 grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' blacklist > blacklist-ip
 grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}[-][0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' blacklist > blacklist-ip-range
-sed -i -E "s/[-][0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\./\//" blacklist-ip-range
+iprange blacklist-ip-range > blacklist-ip-cidr
 
-sort blacklist-ip | uniq -u > blacklist-ip-sorted
-sort blacklist-ip-range | uniq -u > blacklist-ip-range-sorted
+sort blacklist-ip-cidr | grep -v '/' |uniq -u >> blacklist-ip # Here are added only single IP's from range file
+sort blacklist-ip | uniq -u > blacklist-ip-sorted # Sort, check and remove duplicates
+sort blacklist-ip-cidr | grep '/' |uniq -u > blacklist-ip-cidr-sorted # Here are only network segments, sorted and removed duplicates
 
 while read IPADDR
 do
-    route add $IPADDR gw 127.0.0.1 lo &
-done < blacklist-ip-range-sorted
+    route add -net $IPADDR gw 127.0.0.1 lo &
+done < blacklist-ip-cidr-sorted
 
 while read IPADDR
 do
     route add $IPADDR gw 127.0.0.1 lo &
 done < blacklist-ip-sorted
 
-rm -f blacklist-ip
-rm -f blacklist-ip-sorted
-rm -f blacklist-ip-range
-rm -f blacklist-ip-range-sorted
+rm blacklist-ip
+rm blacklist-ip-sorted
+rm blacklist-ip-range
+rm blacklist-ip-cidr
+rm blacklist-ip-cidr-sorted
